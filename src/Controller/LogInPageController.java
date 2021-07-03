e7b9e4908836981774f86d6c1fc97d4590542a37
Alireza Jabbari <foghol47@gmail.com>
2021-07-03 14:33:34 +0430

    feat: adding return to menu method

--- src/Controller/LogInPageController.java
+++ src/Controller/LogInPageController.java
@@ -2,9 +2,13 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LogInPageController {
 
@@ -26,8 +30,13 @@ public class LogInPageController {
     }
 
     @FXML
-    void returnToMainMenu(ActionEvent event) {
+    void returnToMainMenu(ActionEvent event) throws Exception{
+        Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/View/MainMenu.fxml"));
 
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
