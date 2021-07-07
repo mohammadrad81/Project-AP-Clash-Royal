5b79a32779aaaa32381f8aec56d9851d011203d7
foghol47 <foghol47@aut.ac.ir>
2021-07-07 13:35:30 +0430

    feat: add sign up view and controller

---
+++ src/Controller/SignupPageController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignupPageController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    private Button signupButton;
+
+    @FXML
+    void returnToMainMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signup(ActionEvent event) {
+
+    }
+
+}
