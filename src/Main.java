bea32238c6ea9865da9bef95fb61489c43821381
foghol47 <foghol47@aut.ac.ir>
2021-07-07 17:15:47 +0430

    feat: add soundTrack

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
@@ -11,6 +15,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/View/MainMenu.fxml"));
         primaryStage.setTitle("Clash Royale");
+        Media media = new Media(new File("src/SoundTracks/menu.mp3").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+        primaryStage.setUserData(mediaPlayer);
         primaryStage.getIcons().add(new Image("/Pictures/icon.png"));
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
