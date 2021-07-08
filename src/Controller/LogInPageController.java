8427b40728fb0d2273cb1ef17787cdfaf7698ffb
foghol47 <foghol47@aut.ac.ir>
2021-07-08 16:36:00 +0430

    feat: complete login controller

--- src/Controller/LogInPageController.java
+++ src/Controller/LogInPageController.java
@@ -1,16 +1,27 @@
 package Controller;
 
+import Users.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class LogInPageController {
+
+    private final String userFileAddress = "./Users/users.bin";
 
     @FXML
     private Button backButton;
@@ -25,7 +36,53 @@ public class LogInPageController {
     private Button logInButton;
 
     @FXML
-    void logIn(ActionEvent event) {
+    private Label errorLabel;
+
+    @FXML
+    void logIn(ActionEvent event) throws Exception{
+        if (usernameTextField.getText().isEmpty()){
+            errorLabel.setText("FILL USERNAME FIELD");
+            errorLabel.setVisible(true);
+            usernameTextField.requestFocus();
+            return;
+        }
+
+        if (passwordTextField.getText().isEmpty()){
+            errorLabel.setText("FILL PASSWORD FIELD");
+            errorLabel.setVisible(true);
+            passwordTextField.requestFocus();
+            return;
+        }
+        String username = usernameTextField.getText().trim();
+        String password = passwordTextField.getText().trim();
+
+        Player player = findPlayer(username, password);
+
+        if (player == null){
+            errorLabel.setText("USERNAME OR PASSWORD IS INCORRECT");
+            errorLabel.setVisible(true);
+            usernameTextField.requestFocus();
+            return;
+        }
+
+        Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/View/UserMenu.fxml"));
+        loader.load();
+
+        UserMenuController userMenuController = loader.getController();
+        userMenuController.setPlayer(player);
+
+        Media media = new Media(new File("src/SoundTracks/menu2.mp3").toURI().toString());
+        ((MediaPlayer) stage.getUserData()).pause();
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+        stage.setUserData(mediaPlayer);
+
+        Parent root = loader.getRoot();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
 
@@ -37,6 +94,31 @@ public class LogInPageController {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private Player findPlayer(String username, String password){
+        File users = new File(userFileAddress);
+        if (!users.exists())
+            return null;
+
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(users))){
+            while (true){
+                Player player = (Player) objectInputStream.readObject();
+                objectInputStream.readObject();
+                if (player == null)
+                    break;
+                if (player.getUsername().equals(username) && player.getPassword().equals(password))
+                    return player;
+            }
+
+        }
+        catch (IOException e){
+            System.out.println("file not found");
+        }
+        catch (ClassNotFoundException e){
+
+        }
+        return null;
     }
 
 }
