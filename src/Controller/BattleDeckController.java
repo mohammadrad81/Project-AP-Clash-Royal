fc6239dab67f91dae862b6fcc9d3794dd3638d8d
foghol47 <foghol47@aut.ac.ir>
2021-07-08 21:36:43 +0430

    feat: add BattleDeck

---
+++ src/Controller/BattleDeckController.java
@@ -0,0 +1,101 @@
+package Controller;
+
+import Model.Cards.Card;
+import Users.Player;
+import View.CardView;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.util.ArrayList;
+
+public class BattleDeckController {
+
+//    private ArrayList<Card> cards;
+//    private ArrayList<Card> hand;
+    private Player player;
+
+    private ObservableList<Card> cards = FXCollections.observableArrayList();
+    private ObservableList<Card> hand = FXCollections.observableArrayList();
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private ListView<Card> cardsListView;
+
+    @FXML
+    private ListView<Card> handListView;
+
+    @FXML
+    void returnToUserMenu(ActionEvent event) throws Exception{
+        Stage stage = (Stage) backButton.getScene().getWindow();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/View/UserMenu.fxml"));
+        loader.load();
+        UserMenuController controller = loader.getController();
+        controller.setPlayer(player);
+
+        Parent root = loader.getRoot();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void initialize(){
+        cardsListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Card>() {
+            @Override
+            public void changed(ObservableValue<? extends Card> observable, Card oldValue, Card newValue) {
+//                player.getCards().remove(newValue);
+//                player.getHand().add(newValue);
+                cards.remove(newValue);
+                hand.add(newValue);
+
+            }
+        });
+        cardsListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Card>() {
+            @Override
+            public void changed(ObservableValue<? extends Card> observable, Card oldValue, Card newValue) {
+//                player.getCards().add(newValue);
+//                player.getHand().remove(newValue);
+                if (hand.contains(newValue))
+                    hand.remove(newValue);
+                cards.add(newValue);
+
+            }
+        });
+        cardsListView.setCellFactory(new Callback<ListView<Card>, ListCell<Card>>() {
+            @Override
+            public ListCell<Card> call(ListView<Card> param) {
+                return new CardView();
+            }
+        });
+        handListView.setCellFactory(new Callback<ListView<Card>, ListCell<Card>>() {
+            @Override
+            public ListCell<Card> call(ListView<Card> param) {
+                return new CardView();
+            }
+        });
+    }
+
+    public void setPlayer(Player player){
+        this.player = player;
+        cards = FXCollections.observableArrayList(player.getCards());
+        hand = FXCollections.observableArrayList(player.getHand());
+        cardsListView.setItems(cards);
+        handListView.setItems(hand);
+    }
+
+}
---
+++ src/View/CardView.java
@@ -0,0 +1,31 @@
+package View;
+
+import Model.Cards.Card;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class CardView extends ListCell<Card> {
+    private ImageView image = new ImageView();
+
+    public CardView(){
+        image.setFitHeight(95.0);
+        image.setFitWidth(80.0);
+        setPrefHeight(USE_COMPUTED_SIZE);
+        setPrefWidth(USE_COMPUTED_SIZE);
+
+
+    }
+    @Override
+    protected void updateItem(Card item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty || item == null) {
+            setGraphic(null); // don't display anything
+        }
+        else {
+            image.setImage(new Image(item.getCardImageAddress()));
+            setGraphic(image);
+        }
+    }
+}
