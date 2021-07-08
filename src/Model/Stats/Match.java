94e695929d07598aabedd82bb21dca04fd35755c
foghol47 <foghol47@aut.ac.ir>
2021-07-08 16:38:54 +0430

    feat: add Match class

---
+++ src/Model/Stats/Match.java
@@ -0,0 +1,16 @@
+package Model.Stats;
+
+public class Match {
+    private String player1Username;
+    private String player2Username;
+    private int player1Crown;
+    private int player2Crown;
+
+    public Match(String player1Username, String player2Username,
+                 int player1Crown, int player2Crown){
+        this.player1Username = player1Username;
+        this.player2Username = player2Username;
+        this.player1Crown = player1Crown;
+        this.player2Crown = player2Crown;
+    }
+}
