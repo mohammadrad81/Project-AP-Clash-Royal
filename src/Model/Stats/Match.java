b2ddb1505c5791fdc832e55058d00461dc7039ad
foghol47 <foghol47@aut.ac.ir>
2021-07-09 16:41:21 +0430

    feat: add some methods for Match and Player class

--- src/Model/Stats/Match.java
+++ src/Model/Stats/Match.java
@@ -3,6 +3,10 @@ package Model.Stats;
 import java.io.Serializable;
 
 public class Match implements Serializable {
+
+    private static final String victoryImageAddress = "/Pictures/MatchImages/Victory.png";
+    private static final String defeatImageAddress = "/Pictures/MatchImages/Defeat.png";
+
     private String player1Username;
     private String player2Username;
     private int player1Crown;
@@ -14,5 +18,17 @@ public class Match implements Serializable {
         this.player2Username = player2Username;
         this.player1Crown = player1Crown;
         this.player2Crown = player2Crown;
+    }
+
+    @Override
+    public String toString() {
+        return player1Username + " " + player1Crown + " - " + player2Crown + " " + player2Username;
+    }
+
+    public String getImageAddress(){
+        if (player1Crown > player2Crown)
+            return victoryImageAddress;
+        else
+            return defeatImageAddress;
     }
 }
