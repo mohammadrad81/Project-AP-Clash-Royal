675e5b5bbe95cd0157a56187a15732659300b23c
mohammadrad81 <mohammad.rad81@gmail.com>
2021-07-10 21:31:01 +0430

    feat: adding value for speed enum

--- src/Model/Cards/Reals/Troops/Speed.java
+++ src/Model/Cards/Reals/Troops/Speed.java
@@ -4,7 +4,17 @@ package Model.Cards.Reals.Troops;
  * is an enum for speed of the Troop
  */
 public enum Speed {
-    fast,
-    medium,
-    slow,
+    superFast(7),
+    fast(5),
+    medium(4),
+    slow(3);
+
+    private final int value;
+    private Speed(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
